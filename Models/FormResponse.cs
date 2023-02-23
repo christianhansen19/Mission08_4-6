cfe3a69740dbaaf7d590a5d3cde9325ec35d00aa
Colby Jensen <cj433@byu.edu>
2023-02-23 15:29:02 -0700

    Model created.

---
+++ Models/FormResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_4_6.Models
+{
+    public class FormResponse
+    {
+        // Required Fields
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+
+        // Optional Fields
+        public string DueDate { get; set; }
+        public string Category { get; set; }
+        public bool Completed { get; set; }
+
+        // Foreign Key Relationship
+        [Required]
+        public int QuadrantID { get; set; } // QuadrantID from Quadrant Model
+        public Quadrant Quadrant { get; set; }
+    }
+}
---
+++ Models/TaskFormContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_4_6.Models
+{
+    public class TaskFormContext : DbContext
+    {
+        // Constructor
+        public TaskFormContext(DbContextOptions<TaskFormContext> options) : base (options)
+        {
+            // Come back
+        }
+
+        public DbSet<Quadrant> Quadrants { get; set; } // Quadrants Model
+        public DbSet<FormResponse> Responses { get; set; } // Responses Model
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Quadrant>().HasData(
+
+                new Quadrant { QuadrantID = 1, QuadrantName = "Quadrant 1" },
+                new Quadrant { QuadrantID = 2, QuadrantName = "Quadrant 2" },
+                new Quadrant { QuadrantID = 3, QuadrantName = "Quadrant 3" },
+                new Quadrant { QuadrantID = 4, QuadrantName = "Quadrant 4" }
+
+            );
+
+            mb.Entity<FormResponse>().HasData(
+                // Seeded Database
+                new FormResponse
+                {
+                    TaskID = 1,
+                    TaskName = "Homework",
+                    DueDate = "1/1/2023",
+                    Category = "School",
+                    Completed = false,
+                    QuadrantID = 1
+                }
+            );
+        }
+    }
+}
